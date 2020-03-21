4739c29e4dd32a373b86d5b988e29efd735bd19f
Donovan Buck <donovan@donovan.bz>
2020-03-21 12:37:26 -0500

    Update PWM

--- modules/pwm.js
+++ modules/pwm.js
@@ -3,13 +3,16 @@ class PWM {
 	static Output = 0x0a;
 	
   constructor(opts) {
-    this.pin = opts.pin;
-		this.edge = (undefined === opts.edge) ? 0 : opts.edge;
-		this.mode = opts.mode;
-		this.onReadable = opts.onReadable;
-		this.target = opts.target;
-    this.resolution = 10;
-    this.value = null;
+		return (async () => {
+			console.log(opts.pin);
+			this.pin = opts.pin;
+			this.edge = (undefined === opts.edge) ? 0 : opts.edge;
+			this.mode = opts.mode;
+			this.onReadable = opts.onReadable;
+			this.target = opts.target;
+			this.resolution = 10;
+			this.value = null;
+		})();
 	}
   
   read() {
