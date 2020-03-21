85f1d893d2bf9223001a85813eee498442f37773
Donovan Buck <donovan@donovan.bz>
2020-03-21 12:37:14 -0500

    Update digital

--- modules/digital.js
+++ modules/digital.js
@@ -1,22 +1,35 @@
 class Digital {
 	
+	static Input = 0x00;
 	static Output = 0x01;
 
   constructor(opts) {
     this.pin = opts.pin;
 		this.edge = (undefined === opts.edge) ? 0 : opts.edge;
-		this.mode = opts.mode;
+		this.mode = opts.mode || 0x00;
 		this.onReadable = opts.onReadable;
 		this.target = opts.target;
+		this.value = 0x00;
+		this.Input = 0x00;
+		this.Output = 0x01;
 	}
   
   read() {
-		
+		return this.value;
 	}
   
   write(value) {
-		
-  }
+		if (value != this.value) {
+			this.value = value;
+			if (this.mode === this.Input) {
+				this.onReadable();
+			}
+		}
+	}
+	
+	exTrigger(event) {
+		this.emit(event);
+	}
   
 }
 
